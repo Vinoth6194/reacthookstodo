1adde741fdd4d038482a5ca69475cf5d253894d9
vin-unicity <vinoth.kumar@unicity.com>
2020-08-31 20:31:56 +0530

    completed todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,11 +4,24 @@ const Todo = ({ text, todos, setTodos, todo }) => {
   const deleteHandler = () => {
     setTodos(todos.filter((el) => el.id !== todo.id));
   };
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
   return (
     <div>
       <div className="todo">
         <li className="todo-item">{text}</li>
-        <button className="complete-btn">
+        <button className="complete-btn" onClick={completeHandler}>
           <i className="fas fa-check"></i>
         </button>
         <button className="trash-btn" onClick={deleteHandler}>
