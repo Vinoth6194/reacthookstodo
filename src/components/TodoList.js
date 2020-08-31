9a5be3b4d41391aab98c873e5178d0327126ffd4
vin-unicity <vinoth.kumar@unicity.com>
2020-08-31 19:33:33 +0530

    Key added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,7 @@ const TodoList = ({ todos }) => {
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <Todo text={todo.text}></Todo>
+          <Todo text={todo.text} key={todo.id}></Todo>
         ))}
       </ul>
     </div>
